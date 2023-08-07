55a1cd517366d1c4e7297b359e4c2b6763226982
minseong kim <129767606+PoppyPoppyPiggy@users.noreply.github.com>
2023-08-07 17:45:20 +0900

    Update board.php

--- board.php
+++ board.php
@@ -1,3 +1,4 @@
+  GNU nano 6.2                                                                                      board.php                                                                                               
 <?php
  error_reporting( E_ALL );
   ini_set( "display_errors", 1 );
@@ -16,8 +17,8 @@ if (isset($_GET['delete_id'])) {
     $delete_post_id = $_GET['delete_id'];
     $realusername = $_SESSION['realusername'];
 
-    // 게시글 삭제 시 해당 글을 작성한 사용자의 아이디와 현재 로그인한 사용자의 아이디를 비교하>
-    $delete_sql = "UPDATE posts SET deleted = 1 WHERE id = ? AND user_id = (SELECT id FROM user>
+    // 게시글 삭제 시 해당 글을 작성한 사용자의 아이디와 현재 로그인한 사용자의 아이디를 비교하여 일치할 때만 삭제 처리
+    $delete_sql = "UPDATE posts SET deleted = 1 WHERE id = ? AND user_id = (SELECT id FROM users WHERE realusername = ?)";
     $stmt_delete = $mysqli->prepare($delete_sql);
     $stmt_delete->bind_param("is", $delete_post_id, $realusername);
     $stmt_delete->execute();
@@ -31,7 +32,7 @@ $sql = "
         posts.title,
         users.realusername,
         posts.created_at,
-        (SELECT COUNT(*) FROM posts p WHERE p.created_at >= posts.created_at AND p.deleted = 0)>
+        (SELECT COUNT(*) FROM posts p WHERE p.created_at >= posts.created_at AND p.deleted = 0) as post_order
     FROM 
         posts 
     JOIN 
@@ -43,6 +44,7 @@ $sql = "
 ";
 $result = $mysqli->query($sql);
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -86,9 +88,9 @@ $result = $mysqli->query($sql);
     <div class="container">
         <a href="create_post.php" class="create-post-button">Create Post</a>
         <table>
-            <tr>
+           <tr>
                 <th>Post ID</th>
-    <th>Title</th>
+                <th>Title</th>
                 <th>Author</th>
                 <th>Date</th>
                 <th>Actions</th>
@@ -96,11 +98,11 @@ $result = $mysqli->query($sql);
             <?php while ($row = $result->fetch_assoc()): ?>
             <tr>
                 <td><?php echo $row['post_order']; ?></td>
-                <td><a href="view_post.php?id=<?php echo $row['id']; ?>"><?php echo $row['title>
+                <td><a href="view_post.php?id=<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a></td>
                 <td><?php echo $row['realusername']; ?></td>
                 <td><?php echo $row['created_at']; ?></td>
                 <td>w
-                    <?php if (isset($_SESSION['realusername']) && $_SESSION['realusername'] ===>
+                    <?php if (isset($_SESSION['realusername']) && $_SESSION['realusername'] === $row['realusername']): ?>
                         <a href="board.php?delete_id=<?php echo $row['id']; ?>">Delete</a>
                     <?php endif; ?>
                 </td>
@@ -120,8 +122,8 @@ $result = $mysqli->query($sql);
 </body>
 </html>
 
-
-
 <?php
 $mysqli->close();
 ?>
+
+
